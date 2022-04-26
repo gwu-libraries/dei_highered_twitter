c9f9e21fae7fb47d6b68151950c29ab770417396
Dan Kerchner <kerchner@gwu.edu>
2022-04-26 11:33:27 -0400

    Added handling of quotes/replies

--- scripts/extract_full.py
+++ scripts/extract_full.py
@@ -17,7 +17,7 @@ output_file_path = 'full_text.csv'
 
 json_file = open(json_path, 'r', encoding='utf-8')
 
-headers = ['id_str', 'rt_full_text', 'rt_hashtags']
+headers = ['id_str', 'source_full_text', 'source_hashtags']
 output_file = open(output_file_path, 'w', encoding='utf-8')
 output_writer = csv.DictWriter(output_file, fieldnames=headers, lineterminator='\n')
 output_writer.writeheader()
@@ -27,14 +27,24 @@ for line in json_file:
     tweet_out = {}
     tweet_out['id_str'] = tweet['id_str']
 
-    if tweet.get('retweeted_status') is not None:
+    if tweet.get('quoted_status') is not None:
+        # then this is a retweeet
+        qt = tweet.get('quoted_status')
+        if qt.get('extended_tweet') is not None:
+            tweet_out['source_full_text'] = qt['extended_tweet']['full_text'].replace('\n',' ')
+            tweet_out['source_hashtags'] = extract_hashtags(qt['extended_tweet']['entities']['hashtags'])
+        else:
+            tweet_out['source_full_text'] = qt['text'].replace('\n',' ')
+            tweet_out['source_hashtags'] = extract_hashtags(qt['entities']['hashtags'])
+
+    elif tweet.get('retweeted_status') is not None:
         # then this is a retweeet
         rt = tweet.get('retweeted_status')
         if rt.get('extended_tweet') is not None:
-            tweet_out['rt_full_text'] = rt['extended_tweet']['full_text'].replace('\n',' ')
-            tweet_out['rt_hashtags'] = extract_hashtags(rt['extended_tweet']['entities']['hashtags'])
+            tweet_out['source_full_text'] = rt['extended_tweet']['full_text'].replace('\n',' ')
+            tweet_out['source_hashtags'] = extract_hashtags(rt['extended_tweet']['entities']['hashtags'])
         else:
-            tweet_out['rt_full_text'] = rt['text'].replace('\n',' ')
-#            tweet_out['rt_hashtags'] = extract_hashtags(rt['entities']['hashtags']
+            tweet_out['source_full_text'] = rt['text'].replace('\n',' ')
+            tweet_out['source_hashtags'] = extract_hashtags(rt['entities']['hashtags'])
 
     output_writer.writerow(tweet_out)
